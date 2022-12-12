6de17cbbf4cb9e60e10a5203f85bdd47cd048603
Linde Van Wonterghem <linde@codedor.be>
2022-12-12 11:49:17 +0100

    Day 12 visualization

--- 12/index.js
+++ 12/index.js
@@ -4,7 +4,7 @@
   const COL = 1
 
   const init = () => {
-    window.fetch('example.txt').then(data => data.text()).then(data => {
+    window.fetch('data.txt').then(data => data.text()).then(data => {
       let start
       let end
 
@@ -22,26 +22,33 @@
       }))
 
       // PART 1
-      const distancesFromStart = runAlgorithm(map, start)
-      output(1, distancesFromStart[end[ROW]][end[COL]])
+      const resultsFromStart = runAlgorithm(map, start)
+      output(1, resultsFromStart.distances[end[ROW]][end[COL]])
 
       // PART 2
-      const distancesFromEnd = runAlgorithm(map, end, false)
+      const resultsFromEnd = runAlgorithm(map, end, false)
       let shortestDistance = Number.POSITIVE_INFINITY
-      distancesFromEnd.forEach((currentRow, x) => {
+      let closestStart = false
+      resultsFromEnd.distances.forEach((currentRow, x) => {
         currentRow.forEach((distance, y) => {
           if (distance < shortestDistance && map[x][y] === 0) {
             shortestDistance = distance
+            closestStart = [x, y]
           }
         })
       })
       output(2, shortestDistance)
+
+      // VISUALIZE
+      visualize(map, 0, 25, getPath(resultsFromStart.previousSpots, start, end))
+      visualize(map, 0, 25, getPath(resultsFromEnd.previousSpots, end, closestStart), false)
     })
   }
 
   const runAlgorithm = (heights, start, up = true) => {
     const distances = Array.from(Array(heights.length), () => Array.from(Array(heights[0].length), () => Number.POSITIVE_INFINITY))
     const visited = Array.from(Array(heights.length), () => Array.from(Array(heights[0].length), () => false))
+    const previousSpots = Array.from(Array(heights.length), () => Array.from(Array(heights[0].length), () => false))
 
     let current = start
     distances[current[ROW]][current[COL]] = 0
@@ -57,6 +64,7 @@
           if ((up && height - currentHeight <= 1) || (!up && height - currentHeight >= -1)) {
             if (distances[point[ROW]][point[COL]] > distance) {
               distances[point[ROW]][point[COL]] = distance
+              previousSpots[point[ROW]][point[COL]] = current
             }
           }
         })
@@ -77,7 +85,21 @@
       current = lowest
     }
 
-    return distances
+    return { distances, previousSpots }
+  }
+
+  const getPath = (pathVars, start, end) => {
+    const path = []
+    let current = end
+    let fallbackCount = 1000
+
+    while (!(current[ROW] === start[ROW] && current[COL] === start[COL]) && fallbackCount > 0) {
+      path.push(current)
+      current = pathVars[current[ROW]][current[COL]]
+      fallbackCount--
+    }
+
+    return path
   }
 
   const getLocationId = location => `${location[ROW]}-${location[COL]}`
@@ -117,6 +139,48 @@
     $section.appendChild($output)
 
     $container.appendChild($section)
+  }
+
+  const mapNumber = (number, startRange, endRange) => (number - startRange[0]) * (endRange[1] - endRange[0]) / (startRange[1] - startRange[0]) + endRange[0]
+
+  const visualize = (map, lowest, highest, marked, reversePath = true) => {
+    const $container = document.querySelector('.js-visualize')
+    const $map = document.createElement('section')
+
+    map.forEach((row, x) => {
+      const $row = document.createElement('div')
+      $row.classList.add('row')
+
+      row.forEach((tile, y) => {
+        const $pixel = document.createElement('div')
+        $pixel.classList.add('pixel')
+
+        const hue = Math.round(mapNumber(tile, [lowest, highest], [100, 0]))
+        const saturation = Math.round(mapNumber(tile, [lowest, highest], [70, 85]))
+        // const saturation = 0
+        const lightness = Math.round(mapNumber(tile, [lowest, highest], [30, 50]))
+
+        $pixel.classList.add('range')
+        $pixel.style.setProperty('--background', `hsl(${hue}, ${saturation}%, ${lightness}%)`)
+
+        const markedIndex = marked.findIndex(point => point[ROW] === x && point[COL] === y)
+        if (markedIndex > -1) {
+          window.setTimeout(() => {
+            $pixel.classList.add('marked')
+          }, reversePath ? 50 * (marked.length - markedIndex) : 50 * markedIndex)
+          if ((markedIndex === 0) || (markedIndex === marked.length - 1)) {
+            $pixel.classList.add('marked')
+            $pixel.classList.add('big')
+          }
+        }
+
+        $row.appendChild($pixel)
+      })
+
+      $map.appendChild($row)
+    })
+
+    $container.appendChild($map)
   }
 
   init()
